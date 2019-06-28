701eeca2dc1b920250f570761540b5b82a5477ae
Scott Appleton <Scottappleton09@gmail.com>
2019-06-28 00:35:30 +0000

    FINALLY FINISHED THIS

--- eqObjects.js
+++ eqObjects.js
@@ -1,9 +1,9 @@
 const eqArrays = (array1, array2) => {
-  if (array1.length !== array2.length){
+  if (array1.length !== array2.length) {
     return false;
   } else {
-    for(let i = array1.length; i--;){
-      if(array1[i] !== array2[i]){
+    for (let i = array1.length; i--;) {
+      if (array1[i] !== array2[i]) {
         return false;
       }
     }
@@ -14,35 +14,36 @@ const eqArrays = (array1, array2) => {
 const eqObjects = (object1, object2) => {
   let obj1keys = Object.keys(object1).sort();
   let obj2keys = Object.keys(object2).sort();
-  if (obj1keys.length !== obj2keys.length){
+  if (obj1keys.length !== obj2keys.length) {
+    
     return false;
   }
-  for (let i = 0; i < obj1keys.length; i++) {
-    if (Array.isArray(object1[obj1keys[i]]) && Array.isArray(object1[obj2keys[i]])) {
-      if (!eqArrays(object1[obj1keys[i]], object1[obj2keys[i]])) {
+  for (const key in object1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
         return false;
-      } else {
-        if (object1[obj1keys[i]] !== object1[obj2keys[i]]){
-          return false;
-        }
+      }
+    } else {
+      if (object1[key] !== object2[key]) {
+        return false;
       }
     }
   }
-      
   return true;
 };
+  const cd = { c: "1", d: ["2", 3] };
+  const dc = { d: ["2", 3], c: "1" };
+  console.log(eqObjects(cd, dc)); // => true
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+  const ab = { a: "1", b: "2" };
+  const ba = { b: "2", a: "1" };
+  console.log(eqObjects(ab, ba)); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+  const abc = { a: "1", b: "2", c: "3" };
+  console.log(eqObjects(ab, abc)); // => false
 
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+  
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+  const cd2 = { c: "1", d: ["2", 3, 4] };
+  console.log(eqObjects(cd, cd2)); // => false
